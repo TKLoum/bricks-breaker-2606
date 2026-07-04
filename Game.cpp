ff244e9bca37df95fe132950f584043ab5d325c2
TKLoum <tiffanykloum@gmail.com>
2026-07-03 21:13:48 -0400

    TODO 2 Added spacing and clear

--- Game.cpp
+++ Game.cpp
@@ -20,12 +20,13 @@ void Game::Reset()
 	ResetBall();
 
 	// TODO #2 - Add this brick and 4 more bricks to the vector
+	bricks.clear();
 	for (int brickIndex = 0; brickIndex < 5; brickIndex++)
 	{
 		Box brick;
 		brick.width = 10;
 		brick.height = 2;
-		brick.x_position = 0;
+		brick.x_position = brickIndex * 12;
 		brick.y_position = 5;
 		brick.doubleThick = true;
 		brick.color = ConsoleColor::DarkGreen;
