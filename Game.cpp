7db7aa07f2b8adcd6b92d320f3b1db41d02bf6ca
TKLoum <tiffanykloum@gmail.com>
2026-07-03 21:04:15 -0400

    TODO 2 Add 5 bricks to vector

--- Game.cpp
+++ Game.cpp
@@ -20,12 +20,17 @@ void Game::Reset()
 	ResetBall();
 
 	// TODO #2 - Add this brick and 4 more bricks to the vector
-	brick.width = 10;
-	brick.height = 2;
-	brick.x_position = 0;
-	brick.y_position = 5;
-	brick.doubleThick = true;
-	brick.color = ConsoleColor::DarkGreen;
+	for (int brickIndex = 0; brickIndex < 5; brickIndex++)
+	{
+		Box brick;
+		brick.width = 10;
+		brick.height = 2;
+		brick.x_position = 0;
+		brick.y_position = 5;
+		brick.doubleThick = true;
+		brick.color = ConsoleColor::DarkGreen;
+		bricks.push_back(brick);
+	}
 }
 
 void Game::ResetBall()
